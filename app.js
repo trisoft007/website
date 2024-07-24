590215f612fb5df7cc117b3b56ca172cdac49e48
TriSoft <triangularsoftwares@gmail.com>
2024-07-24 12:32:29 +0530

    test without socket no

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const path = require('path');
 const app = express()
-const port = 8080
+const port = 80
 
-app.get('/', (req, res) => {
+// app.set('views', './views')
+// app.set('view engine', 'pug');
+
+/*app.get('/', (req, res) => {
   res.send('Hello World! Welcome to Tri Soft')
-})
+})*/
+
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'views/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
